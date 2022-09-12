8af711ec09b362fe36f88fb7552010b3948a169e
chenzhiqin <chenzq2@allinpay.com>
2022-09-12 15:17:03 +0800

    Initial commit

---
+++ src/main/java/com/example/lelezhutestproject/LelezhutestprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.lelezhutestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LelezhutestprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LelezhutestprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lelezhutestproject/LelezhutestprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lelezhutestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LelezhutestprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
